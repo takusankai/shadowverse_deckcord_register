d015b8b5180b0395b7af3e0d5226557c12f0bd03
takusankai <j021328@gmail.com>
2024-10-29 19:36:23 +0900

    chromeの起動失敗

--- deckcord_resister_flask/deckcord_resister.py
+++ deckcord_resister_flask/deckcord_resister.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import pickle
 import os
 import sys
@@ -28,36 +27,27 @@ def access_portal(deck_code, deck_name):
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--remote-debugging-port=9222')
     options.add_argument(r"--user-data-dir=/mnt/c/Users/takus/AppData/Local/Google/Chrome/User Data")
-    options.add_argument("--profile-directory=Profile 2")
+    options.add_argument("--profile-directory=Profile 5") # どのプロフィールでも上手くいかない
     options.add_argument('--start-maximized')
-    options.add_argument('--lang=ja-JP')  # ロケールを日本語に設定
+    options.add_argument('--lang=ja-JP')
 
-    # ChromeDriverのバージョンを指定
-    driver_path = ChromeDriverManager(driver_version="127.0.6533.120").install()
-    service = Service(driver_path)
+    # ChromeDriverのパスを指
+    # driver_path = ChromeDriverManager(driver_version="127.0.6533.120").install()
+    driver_path = r'/mnt/c/Users/takus/Downloads/develops/chromedriver-win64/chromedriver-win64/chromedriver.exe'
+    service = Service(driver_path, log_path='/mnt/c/Users/takus/Downloads/develops/chromedriver-win64/chromedriver.log')
     driver = webdriver.Chrome(service=service, options=options)
 
-    # driver = webdriver.Chrome(r'C:\Users\takus\chromedriver.exe', options=options)
-    # driver = webdriver.Chrome(executable_path=r'C:\Users\takus\chromedriver.exe', options=options)
+    # 上記がchroniumの起動だが上手くいかない、バージョン変更しても駄目、直にzip展開して指定しても上手く起動せず
+    # とにかく一度たりともgmailすら開けない、ログアウトしてしまう現象も確認
     
     process_deck_input(driver, deck_code, deck_name)
-    # check_portal(driver, deck_code, deck_name)
 
 def process_deck_input(driver, deck_code, deck_name):
     if not re.match(r'^[a-z0-9]{4}$', deck_code):
         return 'エラー: デッキコードは4桁の英数字（小文字限定）ではありませんでした。', 400
 
-    # クッキー情報を読み込む
-    with open('cookie_data.json', 'r') as file:
-        cookies = json.load(file)
-        driver.get(TARGET_URL)  # クッキーを追加する前に一度サイトを開く必要がある
-        for cookie in cookies:
-            if 'sameSite' in cookie and cookie['sameSite'] not in ['Strict', 'Lax', 'None']:
-                del cookie['sameSite']
-            driver.add_cookie(cookie)
-
     driver.get(TARGET_URL)
-    time.sleep(2)  # ページ遷移の待機
+    time.sleep(1)  # ページ遷移の待機
 
     deck_code_input = driver.find_element(By.CSS_SELECTOR, DECK_CODE_INPUT_ID)
     deck_code_input.send_keys(deck_code)
@@ -83,17 +73,6 @@ def process_deck_input(driver, deck_code, deck_name):
 
     # driver.quit()
     return f'{deck_code}を{deck_name}として正常に登録しました！'
-
-def check_portal(driver, deck_code, deck_name):
-
-    time.sleep(5)  # ページ遷移の待機
-
-    driver.get('https://shadowverse-portal.com/signin?lang=ja')
-    time.sleep(2)  # ページ遷移の待機
-
-    google_login_button = driver.find_element(By.XPATH, '//a[@class="signin-button is-google"]')
-    google_login_button.click()
-    time.sleep(2000)  # ページ遷移の待機
 
 @app.route('/process_text', methods=['POST'])
 def process_text():
